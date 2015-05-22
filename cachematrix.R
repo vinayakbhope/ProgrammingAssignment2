9cd41106884c628b2a4fc3d5def9ae2dd55afc0f
vinayakbhope <vinayak.bhope@gmail.com>
2015-05-21 21:58:55 -0400

    Final solution
    final solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function is used to provide cache methods for inverse of a matrix
+## Second function is used to access cache or calculation of inverse of matrix
 
-## Write a short comment describing this function
-
+## makeCacheMatrix provides four methods to set and get the inout values and set and
+# get the inverse of a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        inverseMatrix <- NULL
+        
+        # set method for input
+        setInput <- function(y) {
+                x <<- y
+                inverseMatrix <<- NULL
+        }
+        
+        # get method for inout
+        getInput <- function() {
+                x
+        }
+        
+        # set method for inverse
+        setInverse <- function(inverse_matrix) {
+                inverseMatrix <<- inverse_matrix
+        }
+        
+        # get method for inverse
+        getInverse <- function() {
+                inverseMatrix
+        }
+        
+        list(setInput = setInput, getInput = getInput, setInverse = setInverse, getInverse = getInverse)
+        
 }
 
 
-## Write a short comment describing this function
-
+## This function uses the list output created by makeCacheMatrix function as its input
+## using getInverse function from makeCacheMatrix function it first checks if the inverse
+## is calculated or not. If yes, it is read from get method else inverse is calculated.
+## This calculated inverse is then set into makeCacheMatrix function for caching
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inverseOfMatrix <- x$getInverse()
+        
+        if(!is.null(inverseOfMatrix)) {
+                print("Inverse from cached data")
+                return(inverseOfMatrix)
+        }
+        input_data <- x$getInput()
+        inverseOfMatrix <- solve(input_data)
+        x$setInverse(inverseOfMatrix)
+        return(inverseOfMatrix)
 }
